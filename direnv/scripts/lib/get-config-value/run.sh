0b67b82e2e98c6005599db9f43f6b0676fc67db6
Jake Hamilton <jakehamtexas@gmail.com>
2025-12-05 11:58:50 -0600

    checkpoint - inferred entrypoint

--- direnv/scripts/lib/get-config-value/run.sh
+++ direnv/scripts/lib/get-config-value/run.sh
@@ -2,9 +2,22 @@
 
 export CONFIG_TOML="${CONFIG_TOML}"
 
+dir="$(dirname "$(realpath "${CONFIG_TOML}")")"
+
 case "${1}" in
 entrypoint)
-  tq --raw --file "${CONFIG_TOML}" entrypoint
+  entrypoint="$(tq --raw --file "${CONFIG_TOML}" entrypoint 2>/dev/null || true)"
+
+  if test -n "${entrypoint}"; then
+    echo "${entrypoint}"
+  elif test -f "${dir}"/flake.nix; then
+    echo flake.nix
+  elif test -f "${dir}"/default.nix; then
+    echo default.nix
+  else
+    echo "ERROR: No entrypoint found for config '$(realpath "${CONFIG_TOML}")'" >&2
+    exit 1
+  fi
   ;;
 *)
   echo "Config value '${1}' is invalid" >&2
